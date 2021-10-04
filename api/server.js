f3739da56e54c393888de1820315e60432315470
Pauline122112 <pauline.design007@gmail.com>
2021-10-04 17:00:41 -0500

    made some changes

--- api/server.js
+++ api/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const server = express();
 
-server.use(express.json());
+server.use(express.json())
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found',
+    })
+})
 
 module.exports = server;
