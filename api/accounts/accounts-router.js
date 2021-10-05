3bb591da6cf1c6a245a36a6c50ff984a7f5de1b4
Pauline122112 <pauline.design007@gmail.com>
2021-10-04 20:48:23 -0500

    constantly need to commit

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,20 +1,21 @@
 const router = require('express').Router()
 const md = require('./accounts-middleware')
+const Account = require('./accounts-model')
 
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', async (req, res, next) => {
   try {
-  res.json('get accounts')
+    const accounts = await Account.getAll()
+  res.json(accounts)
   } catch (err) {
     next({ status: 422, message: 'this is horrible'})
   }
 })
 
-router.get('/:id', md.checkAccountId, (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', md.checkAccountId, async (req, res, next) => {
   try {
-    res.json('get accounts by id')
+    const account = await Account.getById(req.params.id)
+    res.json(account)
   } catch (err) {
     next(err)
   }
